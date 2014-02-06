c1880b3e2be4ae8a04188f70383cd21de7cf0768
Jeffrey Ning <jkcn90@gmail.com>
2014-02-06 14:10:25 -0500

    Updated run.py for perceptron for more data

--- perceptron_ps1/run.py
+++ perceptron_ps1/run.py
@@ -30,6 +30,8 @@ split_training_data.run()
 
 # Part 3/4: Train the data on the training set and return the last weight vector. Test the percent
 # error when this weight is run on the validation set
+print('\n=========================================================================================')
+print('Problem 4:')
 (weight_vector,
  total_number_of_misclassifications,
  number_of_runs) = perceptron.perceptron_train(feature_vector_list_training, is_spam_list_training)
@@ -37,10 +39,19 @@ split_training_data.run()
 (feature_vector_list_validation,
  is_spam_list_validation,
  _) = create_feature_vectors.run('./output_data/validation_set', vocabulary_list)
+
+training_set_error = perceptron.perceptron_test(
+                            weight_vector, feature_vector_list_training, is_spam_list_training)
 validation_set_error = perceptron.perceptron_test(
                             weight_vector, feature_vector_list_validation, is_spam_list_validation)
 
+print('Total number of misclassifications: ' + str(total_number_of_misclassifications))
+print('Training set error: ' + str(training_set_error))
+print('Validation set error: ' + str(validation_set_error))
+
 # Part 5: Find words in the vocabulary with the most positive and negative weights
+print('\n=========================================================================================')
+print('Problem 5:')
 sorted_weight_index_least_to_greatest = sorted(range(len(weight_vector)),
                                                key=lambda k: weight_vector[k])
 top_most_positive_weights = [vocabulary_list[index]
@@ -55,8 +66,48 @@ print("Top 15 words with most negative weights: " + str(top_most_negative_weight
 
 # Part 6: Run the averaged perceptron algorithm where the weight vector is the average of all the
 # weight vectors in the run
-(averaged_weight_vector, _, _) = perceptron.perceptron_train_averaged(
+(weight_vector_averaged, _, _) = perceptron.perceptron_train_averaged(
                                     feature_vector_list_training, is_spam_list_training)
+
+# Part 7: Train the perceptron and averaged perceptron algorithm on N = 100, 200, 400, 800, 2000,
+# 4000 rows of the training data. Evaluate the corresponding validation error on all the validation
+# data
+print('\n=========================================================================================')
+print('Problem 7:')
+N = [100, 200, 400, 800, 2000, 4000]
+validation_set_error_n_list = []
+validation_set_error_averaged_n_list = []
+
+for n in N:
+    # Perceptron Algorithm data
+    (weight_vector_n,
+     total_number_of_misclassifications_n,
+     number_of_runs_n) = perceptron.perceptron_train(
+                            feature_vector_list_training, is_spam_list_training,
+                            maximum_number_of_data_to_read=n)
+
+    # Averaged Perceptron Algorithm data
+    (weight_vector_averaged_n,
+     total_number_of_misclassifications_averaged_n,
+     number_of_runs_averaged_n) = perceptron.perceptron_train_averaged(
+                                        feature_vector_list_training, is_spam_list_training,
+                                        maximum_number_of_data_to_read=n)
+
+    # Perceptron Algorithm Validation error
+    validation_set_error_n = perceptron.perceptron_test(
+                                weight_vector_n, feature_vector_list_validation,
+                                is_spam_list_validation)
+    validation_set_error_n_list.append(validation_set_error_n)
+
+    # Averaged Perceptron Algorithm Validation error
+    validation_set_error_averaged_n = perceptron.perceptron_test(
+                                            weight_vector_averaged_n,
+                                            feature_vector_list_validation,
+                                            is_spam_list_validation)
+    validation_set_error_averaged_n_list.append(validation_set_error_averaged_n)
+
+print(validation_set_error_n_list)
+print(validation_set_error_averaged_n_list)
 
 print('\n=========================================================================================')
 print('Script complete')
