5113c5fd5832b8f2d5475df21b56224c61bf3ef7
Jeffrey Ning <jkcn90@gmail.com>
2014-02-19 23:47:43 -0500

    Updated run in learning_support_vector_machines to output graphs

--- learning_support_vector_machines_ps3/pegasos_svm.py
+++ learning_support_vector_machines_ps3/pegasos_svm.py
@@ -12,15 +12,15 @@ def pegasos_svm_test(weight_vector, feature_vector_list, is_spam_list):
         if predicted_value != actual_value:
             number_of_misclassifications += 1
 
-    error = str(number_of_misclassifications / float(feature_vector_list_length))
+    error = number_of_misclassifications / float(feature_vector_list_length)
     return error
 
 def pegasos_svm_train(feature_vector_list, is_spam_list, lambda_, number_of_passes=20,
                       display_intermediate_steps=False):
-    (weight_vectors_list, svm_objective_list) = pegasos_svm_train_raw(
-                                                            feature_vector_list, is_spam_list,
-                                                            lambda_, number_of_passes,
-                                                            display_intermediate_steps)
+    (weight_vectors_list, svm_objective_list) = pegasos_svm_train_raw(feature_vector_list,
+                                                                      is_spam_list, lambda_,
+                                                                      number_of_passes,
+                                                                      display_intermediate_steps)
     return(weight_vectors_list[-1], svm_objective_list)
 
 def pegasos_svm_train_raw(feature_vector_list, is_spam_list, lambda_, number_of_passes,
@@ -40,10 +40,11 @@ def pegasos_svm_train_raw(feature_vector_list, is_spam_list, lambda_, number_of_
         (weight_vector, t) = perceptron_train_iter(weight_vector, feature_vector_list,
                                               is_spam_list, lambda_, t)
 
-        svm_objective = evaluate_svm_objective(weight_vector, feature_vector_list,
+        svm_objective = evaluate_svm_objective(weight_vector, feature_vector_list, 
                                                is_spam_list, lambda_)
         svm_objective_list.append(svm_objective)
         weight_vectors_list.append(weight_vector)
+
         if display_intermediate_steps:
             print('SVM objective: ' + str(svm_objective))
             print('Weight Vector: ' + str(weight_vector))
@@ -66,13 +67,18 @@ def perceptron_train_iter(weight_vector, feature_vector_list, is_spam_list, lamb
     return (weight_vector, t)
 
 def evaluate_svm_objective(weight_vector, feature_vector_list, is_spam_list, lambda_):
-    m = len(feature_vector_list)
-    hinge_loss_error_list = [is_spam_list[i]*np.dot(weight_vector, feature_vector_list[i])
-                             for i in range(0, m)]
-    average_hinge_loss_error = 1/float(m) * sum(hinge_loss_error_list)
+    average_hinge_loss_error = calculate_average_hinge_loss_error(weight_vector, 
+                                                                  feature_vector_list, is_spam_list)
 
     svm_objective = (lambda_ / 2.0)*np.linalg.norm(weight_vector) + average_hinge_loss_error
     return svm_objective
+
+def calculate_average_hinge_loss_error(weight_vector, feature_vector_list, is_spam_list):
+    m = len(feature_vector_list)
+    hinge_loss_error_list = [max(0, 1-is_spam_list[i]*np.dot(weight_vector, feature_vector_list[i]))
+                             for i in range(0, m)]
+    average_hinge_loss_error = 1/float(m) * sum(hinge_loss_error_list)
+    return average_hinge_loss_error
 
 def get_predicted_value(weight_vector, feature_vector):
     dot_product_value = np.dot(weight_vector, feature_vector)
@@ -87,7 +93,7 @@ if __name__ == '__main__':
      vocabulary_list) = create_feature_vectors.run('./output_data/training_set')
 
     (weight_vector, svm_objective_list) = pegasos_svm_train(feature_vector_list,
-                                                                    is_spam_list, pow(2, -5),
-                                                                    display_intermediate_steps=True)
+                                                            is_spam_list, pow(2, -5),
+                                                            display_intermediate_steps=True)
     error = pegasos_svm_test(weight_vector, feature_vector_list, is_spam_list)
     print(error)
--- learning_support_vector_machines_ps3/run.py
+++ learning_support_vector_machines_ps3/run.py
@@ -1,6 +1,8 @@
 import os
 import shutil
 import pylab
+import math
+from operator import itemgetter
 
 # Check that input data exists, clean output data and ensure folder exists
 input_directory = 'input_data'
@@ -26,16 +28,17 @@ import pegasos_svm
 split_training_data.run()
 
 # Transform each email in the training set into a feature vector
-(feature_vector_list,
- is_spam_list,
+(feature_vector_list_training,
+ is_spam_list_training,
  vocabulary_list) = create_feature_vectors.run('./output_data/training_set')
 
 # Part 1a: Plot the svm objective as a function of iterations
-(weight_vector, svm_objective_list) = pegasos_svm.pegasos_svm_train(feature_vector_list,
-                                                                is_spam_list, pow(2, -5))
+(weight_vector, svm_objective_list) = pegasos_svm.pegasos_svm_train(feature_vector_list_training,
+                                                                    is_spam_list_training,
+                                                                    pow(2, -5))
 
-# Plot data
-m = len(feature_vector_list)
+# Plot data ----------------------------------------------------------------------------------------
+m = len(feature_vector_list_training)
 iterations = [(i+1)*m for i in range(0, len(svm_objective_list))]
 
 pylab.plot(iterations, svm_objective_list)
@@ -45,10 +48,102 @@ pylab.ylabel('SVM Objective')
 pylab.title('SVM Objective as a function of iterations')
 pylab.grid(True)
 pylab.savefig("SVM_Objective_Graph.png")
-pylab.show()
+#pylab.show()
 pylab.close()
 pylab.clf()
+# End plot data ------------------------------------------------------------------------------------
 
+# Part 1b/c:
+# Setup validation error data
+(feature_vector_list_validation,
+ is_spam_list_validation,
+ _) = create_feature_vectors.run('./output_data/validation_set', vocabulary_list)
 
+# Setup data for loop
+lambda_set = [pow(2,power) for power in range(-9, 2)]
+
+weight_list = []
+average_training_error_list = []
+average_hinge_loss_error_list = []
+average_validation_error_list = []
+
+# Get data for varying values of lambda
+for lambda_ in lambda_set:
+    (this_weight_vector, _) = pegasos_svm.pegasos_svm_train(feature_vector_list_training,
+                                                            is_spam_list_training, lambda_)
+    weight_list.append(this_weight_vector)
+
+    # Average training error
+    average_training_error = pegasos_svm.pegasos_svm_test(this_weight_vector,
+                                                          feature_vector_list_training,
+                                                          is_spam_list_training)
+    average_training_error_list.append(average_training_error)
+
+    # Average hinge error
+    average_hinge_loss_error = pegasos_svm.calculate_average_hinge_loss_error(
+                                    this_weight_vector, feature_vector_list_training,
+                                    is_spam_list_training)
+    average_hinge_loss_error_list.append(average_hinge_loss_error)
+
+    # Average validation error
+    average_validation_error = pegasos_svm.pegasos_svm_test(this_weight_vector,
+                                                          feature_vector_list_validation,
+                                                          is_spam_list_validation)
+    average_validation_error_list.append(average_validation_error)
+
+# Normalize to percentage out of 100
+average_training_error_list = [100*average_training_error 
+                               for average_training_error 
+                               in average_training_error_list]
+
+average_hinge_loss_error_list = [100*average_hinge_loss_error 
+                               for average_hinge_loss_error 
+                               in average_hinge_loss_error_list]
+
+average_validation_error_list = [100*average_validation_error 
+                                 for average_validation_error 
+                                 in average_validation_error_list]
+
+# Plot data ----------------------------------------------------------------------------------------
+m = len(feature_vector_list_training)
+log_lambda = [math.log(lambda_, 2) for lambda_ in lambda_set]
+
+# Plot logic
+pylab.plot(log_lambda, average_training_error_list)
+pylab.plot(log_lambda, average_hinge_loss_error_list)
+pylab.plot(log_lambda, average_validation_error_list)
+
+pylab.xlabel('log base 2 of lambda')
+pylab.ylabel('Average error (% out of 100')
+pylab.title('Average Errors as a function of log base 2 of lambda')
+pylab.legend(['Training Error', 'Hinge Loss Error', 'Validation Error'], loc=2)
+pylab.grid(True)
+pylab.savefig("Average_Error_Over_Log2_Lambda.png")
+#pylab.show()
+pylab.close()
+pylab.clf()
+# End plot data ------------------------------------------------------------------------------------
+
+# Print results for part 1:
+(minimum_index, minimum_average_validation_error) = min(enumerate(average_validation_error_list),
+                                                        key = itemgetter(1))
+print('The minimum of the validation error was: ' + str(minimum_average_validation_error) + '%')
+
+# Calculate Test Error
+minimum_validation_error_classifier = weight_list[minimum_index]
+
+# Setup test set data
+(feature_vector_list_test,
+ is_spam_list_test,
+ _) = create_feature_vectors.run('./input_data/spam_test.txt', vocabulary_list)
+
+test_set_error = pegasos_svm.pegasos_svm_test(minimum_validation_error_classifier,
+                                              feature_vector_list_test,
+                                              is_spam_list_test)
+
+print('Error on the test set: ' + str(test_set_error*100) + '%')
+
+# Calculate the number of support vectors
+    
 print('\n=========================================================================================')
 print('Script complete')
